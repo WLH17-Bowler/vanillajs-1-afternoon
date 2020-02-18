46e82559f6352d5e5d4c45b7dd9350880699b32f
Josh Bowler <joshbowler46@gmail.com>
2020-02-18 16:23:08 -0700

    completed part2

---
+++ Part2/index.js
@@ -0,0 +1,14 @@
+console.log("The house always wins!")
+
+const cardId = document.getElementById("cardId")
+const cardColor = document.getElementById("cardColor")
+
+const setCard = () => {
+    const card = document.getElementById(cardId.value)
+    card.style.color = cardColor.value
+    // card[]
+    console.log()
+}
+// const clear = () => {
+//     for (let i = 0; i < )
+// }
